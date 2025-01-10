b55697b0ab422d13e5ad0899e1d6b8eec2f7142e
yjtrx <yashrajtambe54@gmail.com>
2025-01-11 00:12:56 +0530

    Revert "added a new function sum which is not correct"
    This reverts commit 0bcb8b574500c5bb92b8a9d9bb3cd74013daf71d.

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,1 @@
 print("Adding all the data processing code related to fraud detection")
-
-a, b = 5, 7
-
-def sum(a, b):
-    c = a+b
-    return a
-
-print('added a new function sum which is not correct')
